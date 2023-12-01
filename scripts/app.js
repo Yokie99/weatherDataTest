40813095dc34c5422ff43db231ae4b006720976a
Yokie99 <kyleym@uci.edu>
2023-11-30 23:16:28 -0800

    removed API

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ let tempMin = document.getElementById("tempMin");
 let pressure = document.getElementById("pressure");
 
 function Fiveday(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.77618697558095&lon=-121.21037327207077&appid=97377d43410276df49e37fbdf9218007&units=imperial')
+    fetch('')
     .then((response)=> {
         return response.json();
     })
